b5976431fcec93297df2046f6da475183c496767
SAMPREETH-R-S <sampreeth2003@gmail.com>
2023-08-28 11:47:14 +0530

    minor change

--- Assignment_3/assgn_3_21CS30038_21CS30059/assgn3_21CS30038_21CS30059_test.c
+++ Assignment_3/assgn_3_21CS30038_21CS30059/assgn3_21CS30038_21CS30059_test.c
@@ -4,9 +4,12 @@ int main() {
     */
     int x = -42;
     float y = 3.14;  //Floating point number
-    char c = 'a';  
+    char c = 'a';
+    char b='\'';
+    char d='\"';  
     enum week{Mon, Tue, Wed, Thur, Fri, Sat, Sun}; //Enumeration
-    char s[20]="Team 23"; //String     
+    char s[20]="Team 23"; //String    
+    char m[20]="\'\"\?\\\a\b\f\n\r\t\v";
     while(y<10) {
         y = y + 1;
     }
