68512bbd0b11749acdd5d070ae718312520055b9
Yash Kumar <yash23kr@kgpian.iitkgp.ac.in>
2023-10-26 14:50:10 +0530

    Added test3

--- Assignment_5/input/test3.c
+++ Assignment_5/input/test3.c
@@ -1,13 +1,25 @@
 //Nested loop and multi-dimension aray
-
-
-int count_components()
-{
-    
-}
-
 int main()
 {
+    int i,j;
+    int arr[6][6];
 
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<6;j++)
+        {
+            arr[i][j] = i+j;
+        }
+    }
+
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            arr[i][j] = arr[j][i];
+        }
+    }
+
+    return 0;
     
 }
